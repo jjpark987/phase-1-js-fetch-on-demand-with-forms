abd8eccffb334bf43e5d110acd66740957af5666
Jason Park <jjpark987@gmail.com>
2022-11-09 13:38:13 -0800

    commit tree 15108646d99812303e35ae2ffc44ba53e5fd7e1e

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 const init = () => {
-  
+  const inputForm = document.querySelector('form');
+
+  inputForm.addEventListener('submit', e => {
+    e.preventDefault();
+
+    fetch(`http://localhost:3000/movies/${e.target.children[1].value}`)
+    .then(r => r.json())
+    .then(d => {
+        const title = document.querySelector('section#movieDetails h4');
+        const summary = document.querySelector('section#movieDetails p');
+
+        title.innerText = d.title;
+        summary.innerText = d.summary;
+    });
+  });
 }
 
 document.addEventListener('DOMContentLoaded', init);
